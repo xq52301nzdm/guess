bf0bd02a01d6dad99d49be50b2daa5e9b1e074de
阴小海 <1742497339@qq.com>
2023-09-26 14:42:20 +0800

    test commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'docs',
-    assetsDir: './'
-  }
+  base: '/guess/',
+	build: {
+		outDir: 'docs',
+		assetsDir: './'
+	}
 })
